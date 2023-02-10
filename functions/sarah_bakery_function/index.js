cf5960d6854e955350a44dc2f8b4632ba8f8bbf5
Buvana-R <124979610+Buvana-R@users.noreply.github.com>
2023-02-10 12:38:59 +0530

    Update index.js

--- functions/sarah_bakery_function/index.js
+++ functions/sarah_bakery_function/index.js
@@ -3,36 +3,39 @@ var express = require('express');
 var app = express();
 var catalyst = require('zcatalyst-sdk-node');
 app.use(express.json());
-const tableName = 'AlienCity'; // The table created in the Data Store
-const columnName = 'CityName'; // The column created in the table
+const tableName = 'bakery'; // The table created in the Data Store
+const columnName = 'FirstName'; // The column created in the table
 
 // The POST API that reports the alien encounter for a particular city
 app.post('/alien', (req, res) => {
- var cityJson = req.body;
+ var fnameJson = req.body.fname;
 console.log(cityJson);
+ 
  // Initializing Catalyst SDK
  var catalystApp = catalyst.initialize(req);
+ 
  // Queries the Catalyst Data Store table and checks whether a row is present for the given city
- getDataFromCatalystDataStore(catalystApp, cityJson.city_name).then(cityDetails => {
-  if (cityDetails.length == 0) { // If the row is not present, then a new row is inserted
-   console.log("Alien alert!"); //Written to the logs. You can view this log from Logs under the Monitor section in the console 
+ getDataFromCatalystDataStore(catalystApp, fnameJson.FirstName).then(fnameDetails => {
+  if (fnameDetails.length == 0) { // If the row is not present, then a new row is inserted
+   console.log("First name missing!"); //Written to the logs. You can view this log from Logs under the Monitor section in the console 
    var rowData={}
    rowData[columnName]=cityJson.city_name;
 
   var rowArr=[];
   rowArr.push(rowData);
+   
    // Inserts the city name as a row in the Catalyst Data Store table
-   catalystApp.datastore().table(tableName).insertRows(rowArr).then(cityInsertResp => {
+   /*catalystApp.datastore().table(tableName).insertRows(rowArr).then(cityInsertResp => {
     res.send({
-     "message": "Thanks for reporting!"
+     "message": "Thanks for updating!"
     });
    }).catch(err => {
     console.log(err);
     sendErrorResponse(res);
    })
-  } else { // If the row is present, then a message is sent indicating duplication
+  }*/ else { // If the row is present, then a message is sent indicating duplication
    res.send({
-    "message": "Looks like you are not the first person to encounter aliens in this city! Someone has already reported an alien encounter here!"
+    "message": "Thanks for revisiting again!"
    });
   }
  }).catch(err => {
@@ -43,8 +46,7 @@ console.log(cityJson);
 
 // The GET API that checks the table for an alien encounter in that city 
 app.get('/alien', (req, res) => {
- var city = req.query.city_name;
-
+ var city = req.query.FirstName;
  // Initializing Catalyst SDK
  var catalystApp = catalyst.initialize(req);
 
